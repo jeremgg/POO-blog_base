eb745167127fb7081e888dbeb0c137dbeabcfc4a
jeremgg <Votre@email.com>
2017-08-25 18:32:56 +0200

    Add the class database that allows to connect to the database

---
+++ app/Database.php
@@ -0,0 +1,62 @@
+<?php
+
+    namespace App;
+
+    use \PDO;   //utiliser la class PDO en dehors du namespace App
+
+
+
+    class Database{
+
+        //Initialiser les paramètres de connexion à la BDD
+        private $db_name;
+        private $db_user;
+        private $db_pass;
+        private $db_host;
+
+        //stocker les paramètres de connexions
+        private $pdo;
+
+
+
+        /**
+         * définir les paramètres de connexion à la BDD
+         * @param string $db_name
+         * @param string $db_user
+         * @param string $db_pass
+         * @param string $db_host
+         */
+        public function __construct($db_name, $db_user = 'root', $db_pass = 'root', $db_host = 'localhost'){
+
+            //Initialiser les paramètres de connexion à la BDD en fonction des paramètre du constructeur
+            $this->db_name = $db_name;
+            $this->db_user = $db_user;
+            $this->db_pass = $db_pass;
+            $this->db_host = $db_host;
+        }
+
+
+
+        /**
+         * Générer le PDO
+         * @return string  la propriété pdo
+         */
+        private function getPDO(){
+
+            //si le connexion à la BDD n'est pas établi
+            if($this->pdo === null){
+                //Créer une instance de l'objet PDO
+                $pdo = new PDO('mysql:dbname=blog;host=localhost', 'root', 'root');
+
+                //Afficher les erreur sql
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                //stocker la fonction dans l'instance
+                $this->pdo = $pdo;
+            }
+
+            return $this->pdo;
+        }
+    }
+
+?>
---
+++ sql/blog.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost:8889
+-- Généré le :  Ven 25 Août 2017 à 18:09
+-- Version du serveur :  5.5.42
+-- Version de PHP :  7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) DEFAULT NULL,
+  `contenu` longtext,
+  `date` datetime DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `articles`
+--
+
+INSERT INTO `articles` (`id`, `titre`, `contenu`, `date`, `category_id`) VALUES
+(1, 'Article de test', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non est igitur voluptas bonum. Hoc dixerit potius Ennius: Nimium boni est, cui nihil est mali. Si mala non sunt, iacet omnis ratio Peripateticorum. Vide igitur ne non debeas verbis nostris uti, sententiis tuis.', '2017-08-01 07:19:00', 2),
+(2, 'Article de test 2', 'Dempta enim aeternitate nihilo beatior Iuppiter quam Epicurus; Duo Reges: constructio interrete. Quasi ego id curem, quid ille aiat aut neget. Nemo igitur esse beatus potest. Illud dico, ea, quae dicat, praeclare inter se cohaerere.', '2017-08-10 05:25:00', 1),
+(3, 'mon titre 3', 'Ut pulsi recurrant? Mihi, inquam, qui te id ipsum rogavi? Nos paucis ad haec additis finem faciamus aliquando; Ergo illi intellegunt quid Epicurus dicat, ego non intellego? Ita relinquet duas, de quibus etiam atque etiam consideret. ', '2017-08-23 08:21:00', 2);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
